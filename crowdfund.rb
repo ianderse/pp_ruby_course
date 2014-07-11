32278bc005b26ac9ef1dcd409e3b685060eacbd5
Ian Andersen <ianderse@mac.com>
2014-07-11 15:28:56 -0600

    end of section 7

--- crowdfund.rb
+++ crowdfund.rb
@@ -1,11 +1,8 @@
-project1 = "ABC"
-project2 = "LMN"
-project3 = "XYZ"
+def print_project(name, funding)
+	"Project #{name} has $#{funding} dollars available as of #{time}"
+end
 
-funding1 = 1000
-funding2 = 5000
-funding3 = 10000
-
-puts "Project #{project1} has $#{funding1} in funding"
-
-puts "Projects:\n\t#{project1}\n\t#{project2}\n\t#{project3}"
+def time
+	t = Time.new
+	t.strftime("%T")
+end
--- studio_game.rb
+++ studio_game.rb
@@ -1,21 +1,10 @@
-name1 = "larry"
-name2 = "curly"
-name3 = "moe"
-name4 = "shemp"
+def say_hello(name, health=100)
+	"I'm #{name.capitalize} with a health of #{health} as of #{time}."
+end
 
-health1 = 30
-health2 = 60
-health3 = 100
-health4 = 90
+def time
+	t = Time.new
+	t.strftime("%T")
+end
 
-current_time = Time.new
-formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
-
-puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
-
-puts "#{name1.capitalize}'s health is #{health1}"
-puts "#{name2.upcase} has a health of #{health2}"
-puts "#{name3.capitalize} has a health of #{health3}".center(50, '*')
-puts "#{name4.capitalize}" + "#{health4} health".rjust(30, '.')
-
-puts "The game started on #{formatted_time}"
+puts say_hello('moe')
