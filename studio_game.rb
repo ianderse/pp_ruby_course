ed2a70bc3820c0ae7e57989aef3633696c6d8dd2
Ian Andersen <ianderse@mac.com>
2014-07-11 15:56:40 -0600

    end of section 7

--- studio_game.rb
+++ studio_game.rb
@@ -1,10 +1,28 @@
-def say_hello(name, health=100)
-	"I'm #{name.capitalize} with a health of #{health} as of #{time}."
+class Player
+	def initialize(name, health=100)
+		@name = name.capitalize
+		@health = health
+	end
+
+	def to_s
+		"I'm #{@name} with a health of #{@health}."
+	end
+
+	def blam
+		@health -= 10
+		puts "#{@name} got blammed!"
+	end
+
+	def w00t
+		@health += 15
+		puts "#{@name} got w00ted!"
+	end
 end
 
-def time
-	t = Time.new
-	t.strftime("%T")
-end
+player1 = Player.new("moe")
+player2 = Player.new("larry", 60)
+player3 = Player.new("curly", 125)
 
-puts say_hello('moe')
+puts player1
+puts player2
+puts player3
