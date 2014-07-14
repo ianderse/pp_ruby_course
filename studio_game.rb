5a920b9f45f4aa99600fa3d5f88d62a4027ede1f
Ian Andersen <ianderse@mac.com>
2014-07-14 11:19:43 -0600

    end of section 10

--- studio_game.rb
+++ studio_game.rb
@@ -30,21 +30,41 @@ class Player
 	end
 end
 
+class Game
+
+	attr_reader :title
+
+	def initialize(title)
+		@players = []
+		@title = title.capitalize
+	end
+
+	def add_player(player)
+		@players.push(player)
+	end
+
+	def play
+		puts "There are #{@players.size} players in #{@title}."
+
+		@players.each do |player|
+			puts player
+		end
+
+		@players.each do |player|
+			player.blam
+			player.w00t
+			player.w00t
+			puts player
+		end
+	end
+end
+
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
-player4 = Player.new("shemp", 90)
 
-players = [player1, player2, player3]
-
-puts "There are #{players.size} players in the game."
-
-players.pop
-players.push(player4)
-
-players.each do |player|
-	player.blam
-	player.w00t
-	player.w00t
-	puts player
-end
+knuckleheads = Game.new("knuckleheads")
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
