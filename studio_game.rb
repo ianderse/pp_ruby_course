f90a992feacb394ec7f1e9050787d267c9e847f9
Ian Andersen <ianderse@mac.com>
2014-07-14 11:03:46 -0600

    end of section 8

--- studio_game.rb
+++ studio_game.rb
@@ -1,11 +1,14 @@
 class Player
+	attr_accessor :name
+	attr_reader :health
+
 	def initialize(name, health=100)
 		@name = name.capitalize
 		@health = health
 	end
 
 	def to_s
-		"I'm #{@name} with a health of #{@health}."
+		"I'm #{@name} with a health of #{@health} and a score of #{score}."
 	end
 
 	def blam
@@ -16,6 +19,14 @@ class Player
 	def w00t
 		@health += 15
 		puts "#{@name} got w00ted!"
+	end
+
+	def score
+		@name.length + @health
+	end
+
+	def name=(new_name)
+		@name = new_name.capitalize
 	end
 end
 
