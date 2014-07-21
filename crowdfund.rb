a460a1b165ca8fd77c6dacdcb8eae4c841febc6d
Ian Andersen <ianderse@mac.com>
2014-07-21 15:24:27 -0600

    adjust spacing

--- crowdfund.rb
+++ crowdfund.rb
@@ -1,8 +1,8 @@
 def print_project(name, funding)
-	"Project #{name} has $#{funding} dollars available as of #{time}"
+  "Project #{name} has $#{funding} dollars available as of #{time}"
 end
 
 def time
-	t = Time.new
-	t.strftime("%T")
+  t = Time.new
+  t.strftime('%T')
 end
